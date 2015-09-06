50cd1b8fb4d1bf0e2fa2b182b5770bb9fb1c051c
Richard Lewis <richrd.lewis@gmail.com>
2015-09-06 18:49:06 +0300

    Added launcher script.

---
+++ bx.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Convenience wrapper for running bx directly from source tree."""
+
+from bx.main import main
+
+if __name__ == '__main__':
+    main()
