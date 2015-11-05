a6ce1432fc43a5f486a25ff0b1b3a443f5953d4a
Richard Lewis <richrd.lewis@gmail.com>
2015-11-05 18:17:06 +0200

    More methods, window type checking, better event handling and bookkeeping etc.

--- bx/bot_windows.py
+++ bx/bot_windows.py
@@ -33,6 +33,12 @@ class Window:
             return self.user.get_nick()
         return self.name
 
+    def is_channel(self):
+        return self.zone == irc_constants.ZONE_CHANNEL
+
+    def is_query(self):
+        return self.zone == irc_constants.ZONE_QUERY
+
     def add_message(self, msg_obj):
         self.messages.append(msg_obj)
 
@@ -81,7 +87,6 @@ class Channel(Window):
     def __init__(self, bot, name=None):
         Window.__init__(self, bot, name)
         self.zone = irc_constants.ZONE_CHANNEL
-
         # Channel modes
         self.modes = []
         # Channel creation time
@@ -92,9 +97,61 @@ class Channel(Window):
         self.topic_by = None
         # Channel topic set time
         self.topic_time = None
-
         # All users on the channel
         self.users = {}
+        # If we're on the channel or not
+        self.joined = 0
+
+    def get_joined(self):
+        return self.joined
+
+    def set_current_modes(self, modes):
+        self.modes = modes
+
+    def set_changed_modes(self, modes):
+        target = self.add_mode
+        for char in modes:
+            if char == "+":
+                target = self.add_mode
+            elif char == "-":
+                target = self.remove_mode
+            else:
+                target(char)
+
+    def set_changed_user_modes(self, modes):
+        for item in modes:
+            print(item)
+            user = self.bot.get_user(item[0])
+            mode = item[1]
+            oper = item[2]
+            func = [self.remove_user_mode, self.add_user_mode][oper]
+            func(user, mode)
+
+    def add_mode(self, mode):
+        if mode not in self.modes:
+            self.modes.append(mode)
+            return True
+        return False
+
+    def remove_mode(self, mode):
+        if mode in self.modes:
+            self.modes.pop(self.modes.index(mode))
+            return True
+        return False
+
+    def get_modes(self):
+        return self.modes
+
+    def get_user_modes(self, user):
+        return self.users[user]["modes"]
+
+    def add_user_mode(self, user, mode):
+        if mode not in self.get_user_modes(user):
+            self.users[user]["modes"].append(mode)
+
+    def remove_user_mode(self, user, mode):
+        if mode in self.get_user_modes(user):
+            self.users[user]["modes"].pop(self.users[user]["modes"].index(mode))
 
     def has_user(self, user):
         return user in self.users.keys()
@@ -107,9 +164,10 @@ class Channel(Window):
             self.logger.error("Trying to call add_user with a nick instead of a user instance!")
             return False
         if user not in self.users.keys():
-            user_data = {"modes": []}
+            modes = []
             if mode:
-                user_data["modes"].append(mode)
+                modes = [mode]
+            user_data = {"modes": modes}
             self.users[user] = user_data
             return True
         self.logger.warning("Trying add existing user to channel!")
@@ -125,6 +183,15 @@ class Channel(Window):
         self.logger.warning("Trying to remove non-existing user from channel!")
         return False
 
+    def ask_modes(self):
+        self.bot.irc.ask_channel_modes(self.get_name())
+
+    def clear_state(self):
+        self.logger.debug("Clearing window state.")
+        self.users = {}
+        self.modes = []
+        self.joined = 0
+
     def clear_users(self):
         self.users = {}
 
@@ -134,25 +201,35 @@ class Channel(Window):
             self.remove_user(event.user)
         if event.name == "on_disconnect":
             # Clear all users on disconnect (cant do bookkeeping)
-            self.clear_users()
+            self.clear_state()
         # Event that requires the current channel
         if event.window == self:
+            if event.name == "on_i_joined":
+                self.joined = 1
             if event.name == "on_channel_join":
                 self.add_user(event.user)
-            elif event.name == "on_channel_part":
+            elif event.name in ["on_channel_part", "on_channel_kick"]:
                 self.remove_user(event.user)
-            elif event.name == "on_channel_kick":
-                self.remove_user(event.user)
+                if event.user == self.bot.get_bot_user():
+                    event.window.clear_state()
             elif event.name == "on_channel_has_users":
                 # Clear all users since this event declares them
                 self.clear_users()
+                self.logger.debug("on_channel_has_users {}".format(event.irc_args["users"]))
                 for user_item in event.irc_args["users"]:
                     user = self.bot.get_user_create(user_item[0])
+                    user.set_online(1)
                     self.add_user(user, user_item[1])
             elif event.name == "on_channel_topic_is":
                 self.topic = event.data
             elif event.name == "on_channel_topic_changed":
                 self.topic = event.data
+            elif event.name == "on_channel_modes_are":
+                self.set_current_modes(event.modes)
+            elif event.name == "on_channel_modes_changed":
+                self.set_changed_modes(event.modes)
+            elif event.name == "on_channel_user_modes_changed":
+                self.set_changed_user_modes(event.modes)
 
     def _serialize(self):
         serialized = Window._serialize(self)
@@ -162,6 +239,7 @@ class Channel(Window):
         serialized["topic"] = self.topic
         serialized["topic_by"] = self.topic_by
         serialized["topic_time"] = self.topic_time
+        serialized["joined"] = self.joined
         serialized["users"] = []
         for user in self.users:
             serialized["users"].append([user._serialize(), self.users[user]])
@@ -175,6 +253,7 @@ class Channel(Window):
         self.topic = serialized["topic"]
         self.topic_by = serialized["topic_by"]
         self.topic_time = serialized["topic_time"]
+        self.joined = serialized["joined"]
         self.users = {}
         for user in serialized["users"]:
             self.users[self.bot.get_user(user[0]["nick"])] = user[1]
