f4c485cf6b3c228546d970e6bc2f0dd558d2ca18
Richard Lewis <richrd.lewis@gmail.com>
2015-11-05 15:53:11 +0200

    Added docstrings.

--- bx/bot_main.py
+++ bx/bot_main.py
@@ -5,6 +5,7 @@ The Bot class for handling a single IRC server.
 import __future__
 import os
 import time
+import datetime
 import logging
 
 from . import irc
@@ -13,10 +14,11 @@ from . import bot_event
 from . import bot_user
 from . import bot_windows
 from . import bot_message
+from . import bot_module
 from . import module_loader
 
 # Modules that require reloading
-__reload__ = [irc, irc_constants, bot_event, bot_user, bot_windows, bot_message, module_loader]
+__reload__ = [irc, irc_constants, bot_event, bot_user, bot_windows, bot_message, module_loader, bot_module]
 
 
 class Bot:
@@ -29,9 +31,6 @@ class Bot:
         self.default_reconnect_wait = 5
         self.reconnect_wait = 5
         self.reconnect_wait_increase = 30
-
-        # TODO: Experimental, might wan't to add this to default settings
-        self.remove_window_on_leave = 1
 
         # Setup our logger with module and network name
         self.logger = logging.getLogger("{}[{}]".format(__name__, self.name))
@@ -69,9 +68,13 @@ class Bot:
         if line.find(self.reboot_identifier) != -1:
             self.app.reboot()
         else:
-            irc.IRCClient.parse_received_line(self.irc, line)
+            return irc.IRCClient.parse_received_line(self.irc, line)
 
     def init(self):
+        """Initialize the bot.
+
+        Sets up the IRC client, events and loads modules.
+        """
         self._init()
         self.irc.init()
         self.setup_client()
@@ -79,15 +82,18 @@ class Bot:
         self.load_modules()
 
     def start(self):
+        """Start the bot and connects."""
         self.running = 1
         if not self.irc.connect():
             return False
 
     def stop(self):
+        """Stop running the bot."""
         self.running = 0
         self.irc.disconnect()
 
     def reconnect(self):
+        """Reconnect to the irc server after a disconnect."""
         # TODO: Keep mainloop running and reconnect on a flag. (Allow HTTP to run etc).
         self.logger.debug("Reconnecting in {} seconds...".format(self.reconnect_wait))
         time.sleep(self.reconnect_wait)
@@ -95,10 +101,12 @@ class Bot:
         self.irc.connect()
 
     def mainloop(self):
+        """Run an iteration of the bot mainloop."""
         if self.irc.is_running():
             self.irc.maintain()
 
     def setup_client(self, use_config=True):
+        """Set up the IRC client settings."""
         self.irc.set_host(self.config["host"])
         self.irc.set_port(self.config["port"])
         self.irc.set_nick(self.config["nick"])
@@ -106,9 +114,11 @@ class Bot:
         self.irc.set_realname(self.config["realname"])
 
     def setup_events(self):
+        """Setup event handlers."""
         self.irc.add_event_handler(self.on_irc_event)
 
     def load_modules(self):
+        """Load bot modules."""
         mod_names = self.module_loader.get_available_modules()
         for name in mod_names:
             module = self.module_loader.load_module(name)
@@ -122,6 +132,12 @@ class Bot:
             self.modules[name] = inst
 
     def set_module_options(self, name, instance, options):
+        """Set module options on a module instance.
+
+        :param name: Module name.
+        :param instance: Module instance.
+        :param options: Option dictionary.
+        """
         instance.set_name(name)
         if "zone" in options.keys():
             instance.set_zone(options["zone"])
@@ -130,10 +146,16 @@ class Bot:
         if "throttle_time" in options.keys():
             instance.set_throttle_time(options["throttle_time"])
 
+    def get_name(self):
+        """Return the bots name (servers name in config)."""
+        return self.name
+
     def get_nick(self):
+        """Return the bots current nick."""
         return self.irc.get_nick()
 
     def get_bot_user(self):
+        """Return the user object of the bot."""
         try:
             return self.get_user(self.get_nick())
         except:
@@ -141,12 +163,14 @@ class Bot:
         return False
 
     def get_user(self, nick):
+        """Return a user by nick."""
         for user in self.users:
             if user.get_nick() == nick:
                 return user
         return False
 
     def get_users(self):
+        """Return list of bot users."""
         return self.users
 
     def get_user_create(self, nick):
@@ -157,20 +181,24 @@ class Bot:
         return user
 
     def get_windows(self):
+        """Return list of bot windows."""
         return self.windows
 
     def get_window(self, name):
+        """Return a window by name (channel or query)."""
         for window in self.windows:
             if window.get_name() == name:
                 return window
         return False
 
     def get_module(self, name):
+        """Return a module instance by its name."""
         if name in self.modules.keys():
             return self.modules[name]
         return False
 
     def get_modules(self):
+        """Return list of all module instances."""
         return self.modules.values()
 
     def on_irc_event(self, name, args):
@@ -187,6 +215,10 @@ class Bot:
         # Set users offline
         for user in self.get_users():
             user.set_online(0)
+            user.deauthenticate()
+            if user == self.get_bot_user():
+                # Remove bot user, might join with different nick
+                self.users.remove(user)
         if self.running and self.config["enabled"]:
             self.reconnect()
 
@@ -243,6 +275,9 @@ class Bot:
             self.on_connect_throttled()
         elif event.name == "on_irc_ready":
             self.on_irc_ready()
+        elif event.name == "on_i_joined":
+            # Aquire channel modes
+            event.window.ask_modes()
         elif event.name in ["on_parse_nick_hostname", "on_whois_hostname"]:
             user = self.get_user(event.irc_args["nick"])
             if not user:
@@ -250,10 +285,6 @@ class Bot:
             user.set_hostname(event.irc_args["hostname"])
         elif event.name == "on_privmsg":
             self.on_privmsg(event)
-        elif event.name in ["on_channel_part", "on_channel_kick"]:
-            if event.user == self.get_bot_user():
-                if self.remove_window_on_leave:
-                    self.remove_window(event.window)
         if event.name == "on_nick_changed":
             if event.user:
                 event.user.set_nick(event.irc_args["new_nick"])
@@ -263,52 +294,28 @@ class Bot:
                 )
         self.trigger_event_handlers(event)
 
-    def handle_http_request(self, request, path):
-        window_names = [win.get_name() for win in self.windows]
-        data = "BOT:{} - {}\n".format(self.name, self.get_nick())
-        data += "\n"
-
-        data += "USERS:\n"
-        for user in self.users:
-            acc_name = ""
-            if user.is_authed():
-                acc_name = " "+user.account.get_username()
-            online = ["no", "yes"][user.get_online()]
-            data += "{} [{}{}] online:{} {}\n".format(user, user.get_permission_level(), acc_name, online, user.get_hostname())
-        data += "\n"
-
-        data += "WINDOWS:\n"
-        for win_name in window_names:
-            win = self.get_window(win_name)
-            data += "[ WIN: {} ]\n".format(win_name)
-            if win.zone == irc_constants.ZONE_CHANNEL:
-                data += "TOPIC: {}\n".format(win.topic)
-            win_users = win.get_users()
-            data += "USERS:{}\n".format(list(win_users))
-            data += "\n"
-
-        names = self.app.config.get_account_names()
-        data += "ACCOUNTS:{}\n".format(", ".join(names))
-        data += "MODULES:{}\n".format(", ".join(map(str, self.modules)))
-
-        response = {"data": data}
-        return response
-
     def add_event_handler(self, callback):
+        """Add a callback for receiving events."""
         self.event_handlers.append(callback)
 
     def trigger_event_handlers(self, event):
+        """Trigger all event handlers after bot event handling is done."""
         # Trigger explicitly registered event handlers
         for handler in self.event_handlers:
             handler(event)
         # Trigger event on all modules
-        for module in self.get_modules():
-            module.on_event(event)
+        try:
+            for module in self.get_modules():
+                module.on_event(event)
+        except Exception:
+            self.logger.exception("Failed to trigger module events!")
 
     def auto_join(self):
+        """Join channels specified in server config."""
         self.irc.join_channels(self.config["channels"].keys())
 
     def create_user(self, nick):
+        """Create a user from nick."""
         if self.get_user(nick):
             self.logger.warning("Trying to create an existsing user!")
             return False
@@ -317,6 +324,7 @@ class Bot:
         return user
 
     def create_window(self, name):
+        """Create a window from nick or channel name."""
         if self.get_window(name):
             self.logger.warning("Trying to create an existsing window!")
             return False
@@ -328,14 +336,57 @@ class Bot:
         return window
 
     def remove_window(self, win):
+        """Remove a window. Takes a name or a window object."""
         self.logger.debug("Removing window {}...".format(win))
         if isinstance(win, str):
             win = self.get_window(win)
         if not win:
             self.logger.warning("Trying to remove window that doesn't exist!")
             return False
-        del self.windows[self.windows.index(win)]
+        # del self.windows[self.windows.index(win)]
+        self.windows.remove(win)
         return True
+
+    def handle_http_request(self, request, path):
+        window_names = [win.get_name() for win in self.windows]
+        data = "BOT:{} - {}\n".format(self.name, self.get_nick())
+        data += "\n"
+
+        data += "USERS:\n"
+        for user in self.users:
+            acc_name = ""
+            if user.is_authed():
+                acc_name = user.account.get_username()
+            online = ["?", "!"][user.get_online()]
+            last_active = "????-??-?? ??:??:??"
+            if user.last_active:
+                last_active = datetime.datetime.fromtimestamp(user.last_active).strftime('%Y-%m-%d %H:%M:%S')
+            data += "{} {} {} {} [{}] {}\n".format(last_active, online, user.get_permission_level(), user.get_nick(), acc_name, user.get_hostname())
+        data += "\n"
+
+        data += "WINDOWS:\n"
+        for win_name in window_names:
+            win = self.get_window(win_name)
+            if win.zone == irc_constants.ZONE_CHANNEL:
+                data += "[{} joined:{} modes:{}]\n".format(win_name, win.get_joined(), "".join(win.get_modes()))
+            else:
+                data += "[{}]\n".format(win_name)
+            if win.zone == irc_constants.ZONE_CHANNEL:
+                data += "TOPIC: {}\n".format(win.topic)
+                win_users = win.get_users()
+                parts = ["".join([{"op": "@", "voice": "+"}[mode] for mode in win.get_user_modes(user)])+user.get_nick() for user in win_users]
+                #user_str = ", ".join(["{}{}".format(user, [win.get_user_modes(user)) for user in win_users])
+                user_str = ", ".join(parts)
+                data += "USERS:{}\n".format(user_str)
+            data += "\n"
+
+        names = self.app.config.get_account_names()
+        data += "ACCOUNTS:{}\n".format(", ".join(names))
+        module_strs = ["{}[{}]".format(module.get_name(), module.get_permission_level()) for module in self.get_modules()]
+        data += "MODULES:{}\n".format(", ".join(module_strs))
+
+        response = {"data": data}
+        return response
 
     def _serialize(self):
         serialized = {
@@ -345,6 +396,7 @@ class Bot:
             "users": [user._serialize() for user in self.users],
             "windows": [window._serialize() for window in self.windows],
         }
+        self.windows = []
         return serialized
 
     def _unserialize(self, serialized):
