478b1234bd56ec51eabfd1d1de7e00651d6ec82e
Richard Lewis <richrd.lewis@gmail.com>
2015-11-08 19:57:40 +0200

    Improved logs module with better expiration and tweaks.

--- bx/modules/logs.py
+++ bx/modules/logs.py
@@ -13,10 +13,12 @@ from bx import bot_module
 class Logs(bot_module.BotModule):
     @staticmethod
     def declare():
-        return {"level": 100}
+        return {"level": 10}
 
     def init(self):
         self.log_requests = {}
+        self.max_hits = 15
+        self.max_age = 120
 
     def rand_id(self):
         return binascii.hexlify(os.urandom(8)).decode("utf-8")
@@ -27,16 +29,37 @@ class Logs(bot_module.BotModule):
         if not duration:
             duration = 60*60  # 1 hour by default
         min_time = time.time() - duration
-        self.log_requests[id] = [win, min_time]
+
+        self.log_requests[id] = {
+            "hits": 0,
+            "window": win,
+            "min_time": min_time,
+            "time": time.time()
+        }
+        
         url = self.get_url()
         win.send("{}?{}".format(url, id))
 
-    def on_http_request(self, request):
-        id = request.get_query()
-        if id in self.log_requests.keys():
-            log_req = self.log_requests[id]
+    def get_log_request(self, id):
+        if id not in self.log_requests.keys():
+            return False
+        log_req = self.log_requests[id]
+        if log_req["hits"] == self.max_hits:
             del self.log_requests[id]
-            response = self.generate_response(log_req[0], log_req[1])
+            return False
+        if time.time() - log_req["time"] > self.max_hits:
+            del self.log_requests[id]
+            return False
+        log_req["hits"] += 1
+        return log_req
+
+    def on_http_request(self, request):
+        self.logger.debug("on_http_request {}".format(request))
+        id = request.get_query()
+        log_req = self.get_log_request(id)
+        if log_req:
+            response = self.generate_response(log_req["window"], log_req["min_time"])
+            self.logging.debug("response {}".format(response))
             return response
         else:
             return {"data": "expired"}
@@ -54,8 +77,12 @@ class Logs(bot_module.BotModule):
 
         html = ""
         records = self.get_records(window, min_time)
+        prev_date_str = ""
         for record in records:
             time_str = datetime.datetime.fromtimestamp(record.get_time()).strftime('%H:%M')
+            date_str = datetime.datetime.fromtimestamp(record.get_time()).strftime('%A %d.%m.%Y')
+            if date_str != prev_date_str:
+                html += '<div class="divider">{}</div>'.format(date_str)
             message = record.get_data()
             if record.get_name() != "privmsg":
                 message = record.get_name()
@@ -79,6 +106,5 @@ class Logs(bot_module.BotModule):
             "data": output,
         }
         return response
-
 
 module_class = Logs
