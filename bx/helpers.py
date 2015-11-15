0d15abafc62e308d3f117914458f59b9d642726f
Richard Lewis <richrd.lewis@gmail.com>
2015-11-16 01:45:25 +0200

    Added merge for merging dicts.

--- bx/helpers.py
+++ bx/helpers.py
@@ -1,6 +1,23 @@
 
 import re
+import types 
 import datetime
+
+
+def merge(x,y):
+    # store a copy of x, but overwrite with y's values where applicable
+    merged = dict(x,**y)
+    xkeys = x.keys()
+
+    # if the value of merged[key] was overwritten with y[key]'s value
+    # then we need to put back any missing x[key] values
+    for key in xkeys:
+        # if this key is a dictionary, recurse
+        if isinstance(x[key], dict) and key in y.keys():
+        # if type(x[key]) is types.DictType and y.has_key(key):
+            merged[key] = merge(x[key],y[key])
+
+    return merged
 
 
 def starts(s, what):
