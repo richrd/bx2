75c1c2f66b3385677ac9fa10803079cb76309ab6
Richard Lewis <richrd.lewis@gmail.com>
2015-12-19 17:25:32 +0200

    Added OP reqeust module for quakenet.

---
+++ bx/modules/gainop.py
@@ -0,0 +1,17 @@
+
+from bx import bot_module
+
+
+class GainOp(bot_module.BotModule):
+    """Have the bot request OPs from QuakeNet (if channel has no OPs)."""
+
+    @staticmethod
+    def declare():
+        return {
+            "level": 100,
+        }
+
+    def run_command(self, win, user, data, caller=None):
+        self.bot.irc.privmsg("R","requestop " + win.get_name())
+
+module_class = GainOp
