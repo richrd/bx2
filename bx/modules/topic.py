919a2c2bbfe0e54e2b281fcda083a8c3e1382b21
Richard Lewis <richrd.lewis@gmail.com>
2016-02-08 22:41:04 +0200

    Better permission handling.

--- bx/modules/topic.py
+++ bx/modules/topic.py
@@ -7,9 +7,12 @@ class Topic(bot_module.BotModule):
 
     @staticmethod
     def declare():
-        return {"level": 100}
+        return {"level": 10}
 
     def run_command(self, win, user, data, caller=None):
+        if not win.is_trusted(user):
+            win.send("sorry, you can't do that")
+            return False
         win.change_topic(data)
 
 module_class = Topic
